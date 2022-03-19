e8a1ced2ec2971c70f6e15e28b45d67f9c1991c1
Ocean272 <ocean272@hotmail.com>
2022-03-19 12:08:05 +0800

    Trying out functional com

---
+++ React/tenarypractice/src/App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import { useState } from 'react';
+import lightOn from './assets/images/light-on.png';
+import lightOff from './assets/images/light-off.png';
+import './App.css';
+
+function App() {
+
+    const [toggle, setToggle] = useState(false);
+
+    const toggleFunc = () => {
+      setToggle(!toggle)
+    }
+      
+    console.log("Try toggle");
+
+  return (
+    <div className="App">
+      {toggle ? lightOff : lightOn}
+      <button onClick={ toggleFunc}>Toggle</button>
+    </div>
+  );
+}
+
+export default App;
